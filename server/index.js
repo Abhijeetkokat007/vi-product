25806c5faefe782cb8e8151c55ddd30fc9d586f9
Abhijeet Kokat <118294497+Abhijeetkokat007@users.noreply.github.com>
2025-06-12 00:00:50 +0530

    Update index.js 4

--- server/index.js
+++ server/index.js
@@ -89,13 +89,13 @@ app.delete('/delete-voting/:id', deleteVoting);
 app.post('/api/contact', submitContactForm);
 
 // === Production build serving ===
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+//   });
+// }
 
 // === Start server ===
 const PORT = process.env.PORT || 5000;
